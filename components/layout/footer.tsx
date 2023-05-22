c5ac0d744a92b38bb31d54123624bba077172cf1
Seth Snel <s.snel@s-bb.nl>
2023-05-22 15:29:38 +0200

    fixed img typo

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -12,7 +12,7 @@ const Footer = ({ selectInviteType }: { selectInviteType: (type: inviteType) => 
       inviteType !== null && <p>Ik ben: {inviteType === 'day' ? 'Daggast' : 'Avondgast'} (<a onClick={() => selectInviteType(null)}>wijzig</a>)</p>
     } */}
     <div className={styles.footerImage}>
-      <Image src="/seth-en-annemijn.jpg" alt="Seth en Annemijn" width="250" height="250" objectFit='cover' />
+      <Image src="/seth-en-annemijn.JPG" alt="Seth en Annemijn" width="250" height="250" objectFit='cover' />
     </div>
   </footer>
 }
